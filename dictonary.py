656080cd94265f77bcd6b1a8de378e0814c05f9c
Sunny2686 <sunnykumar26sunnykumar@gmail.com>
2021-01-21 15:21:22 +0530

    main

--- dictonary.py
+++ dictonary.py
@@ -6,17 +6,30 @@ data = json.load(open("data.json"))
 def translate(word):
     
     word = word.lower()
-    if word in data:
+        
+    def returning_method(searched_word):
         print('Available definations: ' + '\n' )
-        for i,line in enumerate(data[word], start=1):
+        for i,line in enumerate(data[searched_word], start=1):
             print('{}.{}'.format(i, line))
         return
+
+    if word in data:
+        return returning_method(word)
+    
+    if word.title() in data:
+        return returning_method(word.title())
+    
+    if word.upper() in data:
+        return returning_method(word.upper())
+    
     elif len(difflib.get_close_matches(word, data.keys(), cutoff=0.85)) > 0:
         print(difflib.get_close_matches(word, data.keys(), cutoff=0.85))
         yes_no_input = input(f'Did you mean "{difflib.get_close_matches(word, data.keys(), cutoff=0.85)[0]}" "yes/y" or "no/n"? ')
+        
         if yes_no_input.lower() in ['yes', 'y','ye']:
             #Calling function recursively
             translate("{}".format(difflib.get_close_matches(word, data.keys(), cutoff=0.85)[0]))
+            
         elif yes_no_input.lower() in ['no', 'n'] :  
             print('Please double check your entery.')
             return
@@ -24,10 +37,7 @@ def translate(word):
             print(f'Dictonary does not understand the word "{word}"') 
             
     else:
-        if int(word) or float(word):
-            print('Please enter the correct word.')
-        else:
-            print(f'Word "{word}" not found in dictonary.')
+        print(f'Word "{word}" not found in dictonary.')
 
 
 user_search = input('Enter the word to search = ')
