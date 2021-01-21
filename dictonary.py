f9eb20f3e081d75af025d6160d10ba81c2b2daba
Sunny2686 <sunnykumar26sunnykumar@gmail.com>
2021-01-21 13:59:07 +0530

    main

--- dictonary.py
+++ dictonary.py
@@ -1,8 +1,22 @@
 import json
+import difflib
 
 data = json.load(open("data.json"))
 
 def translate(word):
-    for i,line in enumerate(data[word], start=1):
-        print('{}.{}'.format(i, line))
-            
+    word = word.lower()
+    if word in data:
+        for i,line in enumerate(data[word], start=1):
+            print('{}.{}'.format(i, line))
+        return
+    elif len(difflib.get_close_matches(word, data.keys(), cutoff=0.85)) > 0:
+        print(difflib.get_close_matches(word, data.keys(), cutoff=0.85))
+        yes_no_input = input(f'Did you mean "{difflib.get_close_matches(word, data.keys(), cutoff=0.85)[0]}" "yes/y" or "no/n"? ')
+        if yes_no_input.lower() in ['yes', 'y','ye']:
+            #Calling function recursively
+            translate("{}".format(difflib.get_close_matches(word, data.keys(), cutoff=0.85)[0]))
+        elif yes_no_input.lower() in ['no', 'n'] : return
+    else:
+        print(f'Word "{word}" not found in dictonary.')
+
+translate('baby')
