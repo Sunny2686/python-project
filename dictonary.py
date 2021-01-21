19c02465aecb110812a4ae11be8b75471dee4fda
Sunny2686 <sunnykumar26sunnykumar@gmail.com>
2021-01-21 11:11:46 +0530

    main

--- dictonary.py
+++ dictonary.py
@@ -1,9 +1,8 @@
 import json
 
-data = json.loads(open("data.json"))
+data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
-
-word = input('Enter word ')
-translate(word)
+    for i,line in enumerate(data[word], start=1):
+        print('{}.{}'.format(i, line))
+            
